9311688bd3cea2de03dd28bf09461c92b17dd5de
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-04-15 16:56:39 -0500

    MVP

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,12 +1,23 @@
+import random
+from graph import Graph
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
+    def __str__(self):
+        return f"{self.name}"
+
 
 class SocialGraph:
     def __init__(self):
         self.last_id = 0
         self.users = {}
         self.friendships = {}
+
+    def __str__(self):
+        return f"{self.users}"
 
     def add_friendship(self, user_id, friend_id):
         """
@@ -32,10 +43,8 @@ class SocialGraph:
         """
         Takes a number of users and an average number of friendships
         as arguments
-
         Creates that number of users and a randomly distributed friendships
         between those users.
-
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
@@ -43,10 +52,31 @@ class SocialGraph:
         self.users = {}
         self.friendships = {}
         # !!!! IMPLEMENT ME
-
         # Add users
-
+        # Use add_user num_users times
         # Create friendships
+        for i in range(0, num_users):
+            self.add_user(f"User {i+1}")
+        # Generate all friendship combinations
+        possible_friendships = []
+        # Avoid dupes by making sure first number is smaller than second
+        for user_id in self.users:
+            for friend_id in range(user_id+1, self.last_id+1):
+                possible_friendships.append((user_id, friend_id))
+        # Shuffle all possible friendships
+        random.shuffle(possible_friendships)
+        # Create for first X pairs x is total //2
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
+        # * Hint 1: To create N random friendships, you could create a
+        # list with all possible friendship combinations, shuffle the
+        # list, then grab the first N elements from the list. You will
+        # need to `import random` to get shuffle.
+        # * Hint 2: `add_friendship(1, 2)` is the same as
+        # `add_friendship(2, 1)`. You should avoid calling one after
+        # the other since it will do nothing but print a warning. You
+        # can avoid this by only creating friendships where user1 < user2.
 
     def get_all_social_paths(self, user_id):
         """
@@ -57,8 +87,19 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
+
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        print(self.friendships)
+        graph = Graph()
+        for id in self.friendships:
+            graph.add_vertex(id)
+        for path in self.friendships:
+            for friend in self.friendships[path]:
+                graph.add_edge(path, friend)
+        for id in self.friendships:
+            if graph.bfs(id, user_id) is not None:
+                visited[id] = graph.bfs(id, user_id)
         return visited
 
 
